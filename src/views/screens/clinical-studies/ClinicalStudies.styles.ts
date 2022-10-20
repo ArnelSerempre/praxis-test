a3472687c6317c19e62ec8468ece21ae14ac4299
Anyi Lozano <anyisithalozano12@gmail.com>
2022-10-19 21:57:03 -0500

    changes

--- src/views/screens/clinical-studies/ClinicalStudies.styles.ts
+++ src/views/screens/clinical-studies/ClinicalStudies.styles.ts
@@ -2,5 +2,5 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 export const StyledContainer = styled.div`
-    ${tw`mt-[27%] sm:mt-[27%] md:mt-[15%]`}
+    ${tw`mt-[27%] sm:mt-[27%] md:mt-[180px]`}
 `;
