53ed1e82c0420933b9caf7beb2c3c968aa29553b
Anyi Lozano <anyisithalozano12@gmail.com>
2022-10-19 21:59:58 -0500

    kilo

--- src/views/screens/clinical-studies/ClinicalStudies.styles.ts
+++ src/views/screens/clinical-studies/ClinicalStudies.styles.ts
@@ -2,5 +2,5 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 export const StyledContainer = styled.div`
-    ${tw`mt-[27%] sm:mt-[27%] md:mt-[180px]`}
+    ${tw`mt-[140px] sm:mt-[170px] md:mt-[180px]`}
 `;
